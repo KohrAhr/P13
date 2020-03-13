c95e360390b3a8603997f73bc154d79582701cc3
KohrAhr <github@1click.lv>
2020-03-13 23:12:40 +0200

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace Call2020
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.notifyIcon2 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.notifyIcon3 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "PIN Code:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(15, 35);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.PasswordChar = '*';
+            this.textBox1.Size = new System.Drawing.Size(158, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.button1.Location = new System.Drawing.Point(57, 61);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Proceed";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.ContextMenuStrip = this.contextMenuStrip1;
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "Running";
+            // 
+            // notifyIcon2
+            // 
+            this.notifyIcon2.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon2.Icon")));
+            this.notifyIcon2.Visible = true;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem1});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(104, 26);
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(103, 22);
+            this.toolStripMenuItem1.Text = "Close";
+            this.toolStripMenuItem1.Click += new System.EventHandler(this.toolStripMenuItem1_Click);
+            // 
+            // notifyIcon3
+            // 
+            this.notifyIcon3.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon3.Icon")));
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(188, 105);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Welcome!";
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.NotifyIcon notifyIcon2;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.NotifyIcon notifyIcon3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Drawing;
+using System.Net.NetworkInformation;
+using System.Windows.Forms;
+
+namespace Call2020
+{
+    public partial class Form1 : Form
+    {
+        private const string CONST_SERVER_1 = "sdk.1click.lv";
+        private const string CONST_SERVER_2 = "1click.lv";
+        private const string CONST_SERVER_3 = "x2020.1click.lv";
+
+        public bool estproblema = false;
+
+        // 1 sec.
+        private const int CONST_BALLON_DISPLAY_MS = 1000;
+
+        /// <summary>
+        ///     Constructor
+        /// </summary>
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        ///     https://stackoverflow.com/questions/11800958/using-ping-in-c-sharp
+        /// </summary>
+        /// <param name="ip"></param>
+        /// <param name="port"></param>
+        private void ping(string ip)
+        {
+            try
+            {
+                Ping ping = new Ping();
+                PingReply replyPing;
+                replyPing = ping.Send(ip);
+
+                if (replyPing.Status == IPStatus.Success)
+                {
+                    //lblResult.Text = "Ping to " + s.ToString() + "[" + r.Address.ToString() + "]" + " Successful"
+                    //   + " Response delay = " + r.RoundtripTime.ToString() + " ms" + "\n";
+
+                    if (estproblema)
+                    {
+                        ShowGreenOnly();
+                    }
+                    estproblema = false;
+                }
+                else
+                {
+                    estproblema = true;
+                    ShowRedOnly();
+                }
+            }
+            catch
+            {
+                ShowRedOnly();
+                estproblema = true;
+            }
+        }
+
+        private void WaitNotLessThan(int value)
+        {
+            const int CONST_TICK = 50;
+            int c = value;
+
+            while (c > 0)
+            {
+                System.Threading.Thread.Sleep(CONST_TICK);
+                c -= CONST_TICK;
+
+                // https://stackoverflow.com/questions/34613362/what-does-application-processmessage-do
+                Application.DoEvents();
+            }
+        }
+
+        /// <summary>
+        ///     проверка что пароль правильный
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        private bool IsPasswordValid(string value)
+        {
+            return value == "2020" || value == "100500" || value == "666";
+        }
+
+        private void ShowGreenOnly()
+        {
+            notifyIcon3.Visible = false;
+            notifyIcon2.Visible = false;
+            notifyIcon1.Visible = true;
+        }
+
+        private void ShowYellowOnly()
+        {
+            notifyIcon3.Visible = false;
+            notifyIcon2.Visible = true;
+            notifyIcon1.Visible = false;
+        }
+
+        private void ShowRedOnly()
+        {
+            notifyIcon3.Visible = true;
+            notifyIcon2.Visible = false;
+            notifyIcon1.Visible = false;
+        }
+
+        /// <summary>
+        ///     Основная программа
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Verify PIN
+            if (IsPasswordValid(textBox1.Text))
+            {
+                // https://www.c-sharpcorner.com/UploadFile/f9f215/how-to-minimize-your-application-to-system-tray-in-C-Sharp/
+                // Hide main form
+                Hide();
+
+                // Show green Tray icon and hide Yellow
+                ShowGreenOnly();
+
+                // Run ping Cycle 
+                while (true)
+                {
+                    // Ping to 1 IP
+                    ping(CONST_SERVER_1);
+                    // ждём 1 sec
+                    WaitNotLessThan(1000);
+
+                    // Ping to 2 IP
+                    ping(CONST_SERVER_2);
+                    // ждём 1 sec
+                    WaitNotLessThan(1000);
+
+                    // Ping to 3 IP
+                    ping(CONST_SERVER_3);
+                    // ждём 1.5 sec
+                    WaitNotLessThan(1500);
+
+                    // https://stackoverflow.com/questions/34613362/what-does-application-processmessage-do
+                    Application.DoEvents();
+                }
+            }
+
+            // Exit if wrong password. Exit code is 0.
+            Application.Exit();
+        }
+
+        /// <summary>
+        ///     Popup menu -- Выход
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void toolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            // https://stackoverflow.com/questions/13046019/winforms-application-exit-vs-environment-exit-vs-form-close
+            // Force exit with Exit code 1.
+            Environment.Exit(1);
+        }
+    }
+}
